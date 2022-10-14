6c8fe104c801ff305a714ea1c8e3589e17c8b92c
ParthaMalakar <parthamalakar4321@gmail.com>
2022-10-15 00:09:05 +0600

    Task3 Part 2

---
+++ ShopManagement(Add_Cart)/ShopManagement/Controllers/ProductController.cs
@@ -0,0 +1,98 @@
+﻿using ShopManagement.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+
+namespace ShopManagement.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            var db = new MyShopEntities();
+            var product = db.Products.ToList();
+            return View(product);
+            
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Product book)
+        {
+
+            //add book to db
+            var db = new MyShopEntities();
+            db.Products.Add(book);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var db = new MyShopEntities();
+            var book = (from b in db.Products
+                        where b.Id == id
+                        select b).SingleOrDefault();
+            return View(book);
+        }
+        [HttpPost]
+        public ActionResult Edit(Product book)
+        {
+            var db = new MyShopEntities();
+            //
+            //db.Books.Find(book.Id);
+            var ext = (from b in db.Products
+                       where b.Id == book.Id
+                       select b).SingleOrDefault();
+            //ext.Author = book.Author;
+            //ext.Name = book.Name;
+            //ext.Price = book.Price;
+            //ext.Edition = book.Edition;
+
+            db.Entry(ext).CurrentValues.SetValues(book);
+
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Delete(int id) {
+             var db = new MyShopEntities();
+            var book = (from b in db.Products
+                        where b.Id == id
+                        select b).SingleOrDefault();
+             db.Products.Remove(book);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult AddCart(int Id)
+        {
+            var db = new MyShopEntities();
+            var product = (from p in db.Products where p.Id == Id select p).SingleOrDefault();
+            if (Session["cart"] == null)
+            {
+                List<Product> products = new List<Product>();
+                products.Add(product);
+                string json = new JavaScriptSerializer().Serialize(product);
+                Session["cart"] = json;
+            }
+            else
+            {
+                var cart = Session["cart"].ToString();
+                var p = new JavaScriptSerializer().Deserialize<List<Product>>(cart);
+                p.Add(product);
+                string json = new JavaScriptSerializer().Serialize(p);
+                Session["cart"] = json;
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
